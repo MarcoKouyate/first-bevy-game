76c38293c6e4a93c9a0ae2109071b46b180c0db6
Marco Kouyaté <marco.kouyate@yahoo.fr>
2023-11-30 02:50:50 +0100

    First Custom Plugin (Greeting)

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,21 @@ use bevy::prelude::*; //let you import bevy API
 
 fn main() {
     App::new()
-    .add_systems(Startup, add_people)
-    .add_systems(Update, (hello_world, greet_people))
+    .add_plugins((DefaultPlugins, HelloPlugin))
     .run();
 }
+
+// PLUGINS
+pub struct HelloPlugin;
+
+impl Plugin for HelloPlugin {
+    fn build(&self, app: &mut App){
+        app
+        .add_systems(Startup, (hello_world, add_people))
+        .add_systems(Update, greet_people);
+    }
+}
+
 
 // COMPONENTS
 #[derive(Component)]
