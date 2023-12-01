b21f5aea92d18198b234bc264129eee01284b075
Marco Kouyaté <marco.kouyate@yahoo.fr>
2023-12-01 02:44:55 +0100

    Display first UI elements

--- src/main.rs
+++ src/main.rs
@@ -66,11 +66,87 @@ impl Plugin for MemoPlugin {
 }
 
 
+// UI Nodes
+
+fn root_node() -> NodeBundle {
+    NodeBundle {
+        style: Style {
+            width: Val::Percent(100.0),
+            height: Val::Percent(100.0),
+            justify_content: JustifyContent::SpaceBetween,
+            ..default()
+        },
+        ..default()
+    }
+}
+
+fn left_vertical_fill_border() -> NodeBundle {
+    NodeBundle {
+        style: Style {
+            width: Val::Px(200.),
+            border: UiRect::all(Val::Px(2.)),
+            ..default()
+        },
+        background_color: Color::rgb(0.65, 0.65, 0.65).into(),
+        ..default()
+    }
+}
+
+fn left_vertical_fill_content() -> NodeBundle {
+    NodeBundle {
+        style: Style {
+            width: Val::Percent(100.),
+            ..default()
+        },
+        background_color: Color::rgb(0.15, 0.15, 0.15).into(),
+        ..default()
+    }
+}
+
+fn text_ui(asset_server: &Res<AssetServer>) -> TextBundle {
+    TextBundle::from_section(
+        "Text Example",
+        TextStyle {
+            font: asset_server.load("fonts/ShadowsIntoLight-Regular.ttf"),
+            font_size: 30.0,
+            ..default()
+        },
+    ).with_style(Style {
+        margin: UiRect::all(Val::Px(5.)),
+        ..default()
+    })
+}
+
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>){
     commands.spawn(Camera2dBundle::default());
     
     commands.spawn(SpriteBundle{
-        texture: asset_server.load("sprites/traveler_sketch.png"),
+        texture: asset_server.load("sprites/fire.png"),
         ..default()
+    });
+
+    // root node
+    commands
+        .spawn(root_node()).
+        with_children(|parent| {
+            // left vertical fill (border)
+            parent
+                .spawn(left_vertical_fill_border())
+                .with_children(|parent| {
+                    // left vertical fill (content)
+                    parent
+                        .spawn(left_vertical_fill_content())
+                        .with_children(|parent| {
+                            // text
+                            parent.spawn(    (
+                                text_ui(&asset_server)
+                                ,
+                                // Because this is a distinct label widget and
+                                // not button/list item text, this is necessary
+                                // for accessibility to treat the text accordingly.
+                                Label,
+                            ));
+                        });
+                });
     });
 }
