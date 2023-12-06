bd0a3a9013bf267d12e036d10a5b9e6d6816f5e8
Marco Kouyaté <marco.kouyate@yahoo.fr>
2023-12-06 23:18:26 +0100

    Create List from String Vector

--- src/guideschapter.rs
+++ src/guideschapter.rs
@@ -3,7 +3,7 @@ use bevy::{
         accesskit::{NodeBuilder, Role},
         AccessibilityNode,
     },
-    prelude::*,
+    prelude::*, render::render_resource::encase::rts_array::Length,
 };
 
 // PLUGIN
@@ -30,25 +30,11 @@ fn setup_scene(commands: &mut Commands, asset_server: &Res<AssetServer>) {
     });
 }
 
-fn list_item_ui(
-    asset_server: &Res<AssetServer>,
-    index: i32,
-) -> (TextBundle, Label, AccessibilityNode) {
-    (
-        TextBundle::from_section(
-            format!("Item {index}"),
-            TextStyle {
-                font: asset_server.load("fonts/ShadowsIntoLight-Regular.ttf"),
-                font_size: 40.,
-                ..default()
-            },
-        ),
-        Label,
-        AccessibilityNode(NodeBuilder::new(Role::ListItem)),
-    )
-}
 
-fn setup_ui(commands: &mut Commands, asset_server: &Res<AssetServer>) {
+
+fn create_ui_list(title: &str, actions: Vec<String>, commands: &mut Commands, asset_server: &Res<AssetServer>){
+    let height : f32  = 50.0 +  (40.0 * actions.length() as f32 );
+    
     let root_node = NodeBundle {
         style: Style {
             width: Val::Percent(100.0),
@@ -62,7 +48,7 @@ fn setup_ui(commands: &mut Commands, asset_server: &Res<AssetServer>) {
     let left_vertical_fill_border = NodeBundle {
         style: Style {
             width: Val::Px(200.),
-            height: Val::Px(500.),
+            height: Val::Px(height),
             border: UiRect::all(Val::Px(2.)),
             ..default()
         },
@@ -83,7 +69,7 @@ fn setup_ui(commands: &mut Commands, asset_server: &Res<AssetServer>) {
     };
 
     let text_ui = TextBundle::from_section(
-        "Text Example",
+        title,
         TextStyle {
             font: asset_server.load("fonts/ShadowsIntoLight-Regular.ttf"),
             font_size: 30.0,
@@ -120,11 +106,45 @@ fn setup_ui(commands: &mut Commands, asset_server: &Res<AssetServer>) {
                             ..default()
                         }).with_children(|parent| {
                             // List items
-                            for i in 0..5 {
-                                parent.spawn(list_item_ui(asset_server, i));
+                            for action in actions {
+                                parent.spawn(list_item_ui(asset_server, &action));
                             }
                         });
                     });
             });
     });
+}
+
+
+// LIST ITEM
+fn list_item_ui(
+    asset_server: &Res<AssetServer>,
+    action: &String
+) -> (TextBundle, Label, AccessibilityNode) {
+    (
+        TextBundle::from_section(
+            action,
+            TextStyle {
+                font: asset_server.load("fonts/ShadowsIntoLight-Regular.ttf"),
+                font_size: 40.,
+                ..default()
+            },
+        ),
+        Label,
+        AccessibilityNode(NodeBuilder::new(Role::ListItem)),
+    )
+}
+
+fn setup_ui(commands: &mut Commands, asset_server: &Res<AssetServer>) {
+
+    let actions = vec![
+        String::from("Test 1"),
+        String::from("Test 2"),
+        String::from("Test 1"),
+        String::from("Test 2"),
+        String::from("Test 1"),
+        String::from("Test 2"),
+        String::from("Test 3")
+    ];
+    create_ui_list("Example",  actions, commands, asset_server);
 }
