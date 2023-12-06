2fb98f2bd1a3d7c683d5e7c563073d9ed3c73d9e
Marco Kouyaté <marco.kouyate@yahoo.fr>
2023-12-06 22:30:03 +0100

    Render a UI list

--- src/guideschapter.rs
+++ src/guideschapter.rs
@@ -1,45 +1,45 @@
 use bevy::{
-    prelude::*,
     a11y::{
         accesskit::{NodeBuilder, Role},
         AccessibilityNode,
-    }
+    },
+    prelude::*,
 };
 
+// PLUGIN
 pub struct MemoPlugin;
 
 impl Plugin for MemoPlugin {
     fn build(&self, app: &mut App) {
-        app
-        .add_systems(Startup, setup);
+        app.add_systems(Startup, setup);
     }
 }
 
-
-
-fn setup(mut commands: Commands, asset_server: Res<AssetServer>){
+// SETUP
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     setup_scene(&mut commands, &asset_server);
     setup_ui(&mut commands, &asset_server);
 }
 
-fn setup_scene(commands: &mut Commands, asset_server: &Res<AssetServer>){
+fn setup_scene(commands: &mut Commands, asset_server: &Res<AssetServer>) {
     commands.spawn(Camera2dBundle::default());
-    
-    commands.spawn(SpriteBundle{
+
+    commands.spawn(SpriteBundle {
         texture: asset_server.load("sprites/fire.png"),
         ..default()
     });
 }
 
-
-fn list_item_ui( asset_server: &Res<AssetServer>, index: i32) -> (TextBundle, Label, AccessibilityNode ) {
+fn list_item_ui(
+    asset_server: &Res<AssetServer>,
+    index: i32,
+) -> (TextBundle, Label, AccessibilityNode) {
     (
         TextBundle::from_section(
             format!("Item {index}"),
             TextStyle {
-                font: asset_server
-                    .load("fonts/ShadowsIntoLight-Regular.ttf"),
-                font_size: 20.,
+                font: asset_server.load("fonts/ShadowsIntoLight-Regular.ttf"),
+                font_size: 40.,
                 ..default()
             },
         ),
@@ -48,8 +48,8 @@ fn list_item_ui( asset_server: &Res<AssetServer>, index: i32) -> (TextBundle, La
     )
 }
 
-fn setup_ui(commands: &mut Commands, asset_server: &Res<AssetServer>){
-    let root_node =    NodeBundle {
+fn setup_ui(commands: &mut Commands, asset_server: &Res<AssetServer>) {
+    let root_node = NodeBundle {
         style: Style {
             width: Val::Percent(100.0),
             height: Val::Percent(100.0),
@@ -58,8 +58,8 @@ fn setup_ui(commands: &mut Commands, asset_server: &Res<AssetServer>){
         },
         ..default()
     };
-    
-    let  left_vertical_fill_border =     NodeBundle {
+
+    let left_vertical_fill_border = NodeBundle {
         style: Style {
             width: Val::Px(200.),
             height: Val::Px(500.),
@@ -72,6 +72,9 @@ fn setup_ui(commands: &mut Commands, asset_server: &Res<AssetServer>){
 
     let left_vertical_fill_content = NodeBundle {
         style: Style {
+            display: Display::Flex,
+            flex_direction: FlexDirection::Column,
+            align_items: AlignItems::Center,
             width: Val::Percent(100.),
             ..default()
         },
@@ -86,39 +89,42 @@ fn setup_ui(commands: &mut Commands, asset_server: &Res<AssetServer>){
             font_size: 30.0,
             ..default()
         },
-    ).with_style(Style {
+    )
+    .with_style(Style {
         margin: UiRect::all(Val::Px(5.)),
         ..default()
     });
 
-
     // root node
-    commands
-        .spawn(root_node).
-        with_children(|parent| {
-            // left vertical fill (border)
-            parent
-                .spawn(left_vertical_fill_border)
-                .with_children(|parent| {
-                    // left vertical fill (content)
-                    parent
-                        .spawn(left_vertical_fill_content)
-                        .with_children(|parent| {
-                            // text
-                            parent.spawn(    (
-                                text_ui
-                                ,
-                                // Because this is a distinct label widget and
-                                // not button/list item text, this is necessary
-                                // for accessibility to treat the text accordingly.
-                                Label,
-                            )).with_children(|parent| {
-                                // List items
-                                for i in 0..30 {
-                                    parent.spawn(list_item_ui(asset_server, i));
-                                }
-                            });
+    commands.spawn(root_node).with_children(|parent| {
+        // left vertical fill (border)
+        parent
+            .spawn(left_vertical_fill_border)
+            .with_children(|parent| {
+                // left vertical fill (content)
+                parent
+                    .spawn(left_vertical_fill_content)
+                    .with_children(|parent| {
+                        // text
+                        parent.spawn((
+                            text_ui,
+                            Label,
+                        ));
+
+                        parent.spawn(NodeBundle {
+                            style: Style {
+                                display: Display::Flex,
+                                flex_direction: FlexDirection::Column,
+                                ..default()
+                            },
+                            ..default()
+                        }).with_children(|parent| {
+                            // List items
+                            for i in 0..5 {
+                                parent.spawn(list_item_ui(asset_server, i));
+                            }
                         });
-                });
+                    });
+            });
     });
 }
